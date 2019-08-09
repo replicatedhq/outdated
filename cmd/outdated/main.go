bd3c294b3af68352966d678c17d8072a63f1a3ce
divolgin <dmitriy@replicated.com>
2019-08-09 07:55:20 -0700

    adding security package for gcp

--- cmd/outdated/main.go
+++ cmd/outdated/main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/replicatedhq/outdated/cmd/outdated/cli"
 
+// Required to use this tool with GCP
+import _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
 func main() {
 	cli.InitAndExecute()
 }
