833004abc91e1d56cf6e77681bf177fb8816e366
Marc Campbell <marc.e.campbell@gmail.com>
2019-08-16 21:15:07 -0700

    Fix formatting of imports:

--- cmd/outdated/main.go
+++ cmd/outdated/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "github.com/replicatedhq/outdated/cmd/outdated/cli"
-
-// Required to use this tool with GCP
-import _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+import (
+	"github.com/replicatedhq/outdated/cmd/outdated/cli"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
+)
 
 func main() {
 	cli.InitAndExecute()
