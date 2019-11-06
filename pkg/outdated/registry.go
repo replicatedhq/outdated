ecc8bd337fd26c871ebf34c01e6e901701776a19
Marc Campbell <marc.e.campbell@gmail.com>
2019-11-06 17:45:25 +0000

    Handle private images

--- pkg/outdated/registry.go
+++ pkg/outdated/registry.go
@@ -1,6 +1,12 @@
 package outdated
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -9,14 +15,58 @@ import (
 	"github.com/pkg/errors"
 )
 
+type DockerConfig struct {
+	Auths map[string]DockerAuth `json:"auths"`
+}
+
+type DockerAuth struct {
+	Auth string `json:"auth"`
+}
+
 func initRegistryClient(hostname string) (*registry.Registry, error) {
 	if hostname == "docker.io" {
 		hostname = "index.docker.io"
 	}
 
+	username := ""
+	password := ""
+
+	if _, err := os.Stat(path.Join(homeDir(), ".docker", "config.json")); err == nil {
+		data, err := ioutil.ReadFile(path.Join(homeDir(), ".docker", "config.json"))
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to read docker config")
+		}
+
+		dockerConfig := DockerConfig{}
+		if err := json.Unmarshal(data, &dockerConfig); err != nil {
+			return nil, errors.Wrap(err, "failed to parse docker config")
+		}
+
+		for host, auth := range dockerConfig.Auths {
+			useAuth := false
+			if strings.Contains(host, "index.docker.io") && hostname == "index.docker.io" {
+				useAuth = true
+			}
+			if host == hostname {
+				useAuth = true
+			}
+
+			if useAuth {
+				decoded, err := base64.StdEncoding.DecodeString(auth.Auth)
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to decode docker auth")
+				}
+
+				usernameAndPassword := strings.Split(string(decoded), ":")
+				username = usernameAndPassword[0]
+				password = usernameAndPassword[1]
+			}
+		}
+	}
+
 	auth := types.AuthConfig{
-		Username:      "",
-		Password:      "",
+		Username:      username,
+		Password:      password,
 		ServerAddress: hostname,
 	}
 
@@ -54,4 +104,11 @@ func parseTags(tags []string) ([]*semver.Version, []string) {
 	}
 
 	return semverTags, nonSemverTags
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE")
 }
